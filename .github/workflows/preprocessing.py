1f2b7087faa234c53cde69288474af394e483df8
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2020-11-05 16:12:39 +0100

    Integrate script generation

---
+++ .github/workflows/preprocessing.py
@@ -0,0 +1,33 @@
+import glob, os
+import shutil
+import re
+
+blackList = ["{{", "******", "@Rextester", "@Tau", "@LIA.eval"]
+
+for file in glob.glob("*.md"):
+    if file != "README.md":
+        shutil.copy2(file, file+"x")
+
+for file in glob.glob("*.mdx"):
+    content = open(file, 'r').readlines()
+    filtered = []
+    for line in content:
+        if any(entry in line for entry in blackList):
+            continue
+        result = re.search( r'```\S+', line)
+        if result:
+            line = result.group(0) + "\n"
+        filtered.append(line)
+
+    with open(file, "w") as outfile:
+        if file.endswith("00_Einführung.mdx"):  # insert metadata for pandoc in first md file
+            title = "**Eingebettete Systeme - TU Freiberg**"
+            outfile.write(f"---\ntitle: |\n  {title}\n  https://github.com/SebastianZug/VL_EingebetteteSysteme/\nauthor:\n")
+            with open(".github/workflows/authors.txt", "r") as authors:  # read in authors and write them into the yaml code
+                for line in authors:
+                    name = line.strip()
+                    outfile.write(f"  - {name}\n")
+
+            outfile.write("papersize:\n  - a4\ngeometry:\n  - margin=2cm\n")
+            outfile.write("header-includes:\n  - \\usepackage{titling}\n  - \\pretitle{\\begin{center}\n    \\includegraphics[width=2cm]{images/Logo_TU_Freiberg.png}\\LARGE\\\\}\n  - \\posttitle{\\end{center}}\n---\n")
+        outfile.write("".join(filtered))
