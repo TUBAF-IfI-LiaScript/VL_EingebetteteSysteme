59020c643a18f7a115de4b6c23f9cf37e3515dd7
Sebastian Zug <sebastian.zug@informatik.tu-freiberg.de>
2023-10-16 13:26:05 +0200

    Add ultrasonic example

--- exampleCode/00_Einfuehrung/USsensor/USsensor.ino
+++ exampleCode/00_Einfuehrung/USsensor/USsensor.ino
@@ -1,26 +1,41 @@
-const int trigPin = 50;
-const int echoPin = 52;
+#include <Wire.h>
+#include <LiquidCrystal_PCF8574.h>
+#include <NewPing.h>
 
-long duration;
-int distance;
+#define trigPin  12
+#define echoPin  11
+#define MAX_DISTANCE 350 // Maximum sensor distance is rated at 400-500cm.
 
-void setup() {
-  pinMode(trigPin, OUTPUT); 
-  pinMode(echoPin, INPUT); 
+int lcdi2c = 0x27; // <- Hart eingecodete Adresse, die nur für unser
+                   // Beispiel funktioniert, vgl. Datenblatt
+
+LiquidCrystal_PCF8574 lcd(lcdi2c);
+NewPing sonar(trigPin, echoPin, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.setBacklight(255);
+  lcd.clear();
+
   Serial.begin(9600);
+  Serial.print("Los geht's");
 }
 
-void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  // v = 0.034m/s
-  // der Weg wird zwei mal zurückgelegt
-  // s = t x v / 2
-  distance= duration*0.034/2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
+void loop()
+{
+  Serial.print("Dist---->: ");
+  Serial.println(sonar.ping_cm());
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("#CrossLab 2023");
+  lcd.setCursor(0, 1);
+  int value = sonar.ping_cm();
+  if (value >= 100)                   lcd.print("   ");
+  if ((value < 100) && (value >=10))  lcd.print("    ");
+  if (value < 10)                     lcd.print("     ");
+  lcd.print(value);
+  lcd.print(" cm");
+  delay(400);
 }
